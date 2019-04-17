e6c66d30b75b44c1f0cde78f6eb5d6d68fe317df
Edmaniak <adam.ouhrabka@gmail.com>
2019-04-17 10:08:23 +0200

    ladeni

--- src/components/button.js
+++ src/components/button.js
@@ -1,3 +1,5 @@
+//import _button from '../scss/_button.scss';
+
 const button = (price, text, link) => {
 	return `<a href="${link}" class="btn btn--orange btn--big">
 					<span class="btn__price">${price}</span>
